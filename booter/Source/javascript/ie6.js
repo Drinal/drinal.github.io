6889c74a30d135aff4fb17f1928836b850865285
Drinal <rotmgpromarket@gmail.com>
2014-05-08 12:39:02 -0500

    .

--- booter/Source/javascript/ie6.js
+++
@@ -1,9 +0,0 @@
-DD_belatedPNG.fix(".css selector");
-DD_belatedPNG.fix(".twitterBox");
-DD_belatedPNG.fix("img");
-DD_belatedPNG.fix(".welcomeBox a");
-DD_belatedPNG.fix(".whatBox a");
-DD_belatedPNG.fix(".serviceBox a");
-DD_belatedPNG.fix(".ourblogBox a");
-DD_belatedPNG.fix(".companyBox a");
-DD_belatedPNG.fix(".buttons a");
--- booter/cron/cron_job7days.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("_VALID_PHP", true);
-  require_once("../init.php");
-  
-  $member->membershipCron(7);
-?>
